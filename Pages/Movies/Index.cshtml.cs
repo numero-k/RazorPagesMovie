9262bb40cd53381450d63f4dc945d88c04a060bd
김건호 <gunho7084@gmail.com>
2021-08-21 21:58:42 +0900

    study

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -20,11 +21,35 @@ namespace RazorPagesMovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
-
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; } // Contains the text users enter in the search text box. 
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; } // Contains the list of genres.
+        [BindProperty(SupportsGet =true)]
+        public string MovieGenre { get; set; }
         public async Task OnGetAsync() 
             // When a request is made for the page, the OnGetAsync method returns a list of movies to the Razor Page.
             // is called to initialize the state of the page.
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            // using System.Linq;
+            // add search
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                // The Contains method is run on the database, not in the C# code
+                movies = movies.Where(s => s.Title.Contains(SearchString)); //a Lambda Expression
+            }
+            // Search by genre
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
             Movie = await _context.Movie.ToListAsync();
         }
     }
